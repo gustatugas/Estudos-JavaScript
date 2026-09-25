7516a9a1e87bb2e27935a7c19be4085cb23467d8
Gustavo Bezerra <gustavo.b.souza002@gmail.com>
2026-09-25 09:23:36 -0500

    arrays e estruturas

---
+++ 05 - Arrays e Estruturas/aula05-1.js
@@ -0,0 +1,7 @@
+// Crie um programa que dado um número imprima a sua tabuada
+
+const numero = 2
+for (let i = 1; i < 11; i++) {
+    let multiplica = numero * i;
+    console.log(`${numero} x ${i} = ${multiplica}`);
+}
